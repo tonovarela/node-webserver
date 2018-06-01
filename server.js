150a2022348cb66e6691cc74410ae65b974df639
Marco Antonio Estelles <tonovarela@tonovarelamac.local>
2018-06-01 00:15:17 -0500

    Primer commit

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+
+const hbs = require('hbs');
+require('./hbs/helpers');
+
+
+const port =process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerPartials(__dirname + '/views/parciales');
+
+
+
+//Express HBS
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+    res.render('home', {
+        nombre: 'Marco Antonio Estelles Martinez'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+
+
+app.get('/data', (req, res) => {
+    res.send('Hola data');
+});
+
+app.listen(port, () => {
+    console.log(`Escuchando peticiones en el puerto ${port}`);
+}
+);
